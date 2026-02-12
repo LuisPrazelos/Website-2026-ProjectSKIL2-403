5bc38e4df258fe7ac3ef1773702b2f9d98739c46
LuisPrazelos <r1037221@student.thomasmore.be>
2026-02-12 15:30:27 +0100

    Prijs evolutie is klaar

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -36,6 +36,7 @@ class DatabaseSeeder extends Seeder
             NotificationPreferenceSeeder::class,
             DessertSeeder::class,
             SurplusSeeder::class,
+            PriceEvolutionSeeder::class,
         ]);
     }
 }
