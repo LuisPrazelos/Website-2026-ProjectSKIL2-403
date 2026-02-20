ca07b0b0116a06327d340c63caa3a9dc91284760
HenkieFK <r0997833@student.thomasmore.be>
2026-02-20 16:09:19 +0100

    FK-Sidebar + Footer

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -1,5 +1,11 @@
-<x-layouts.app.sidebar :title="$title ?? null">
-    <flux:main>
-        {{ $slot }}
-    </flux:main>
-</x-layouts.app.sidebar>
+<div class="flex min-h-screen flex-col">
+    <div class="flex-1">
+        <x-layouts.app.sidebar :title="$title ?? null">
+            <flux:main>
+                {{ $slot }}
+            </flux:main>
+        </x-layouts.app.sidebar>
+    </div>
+
+    <x-layouts.app.footer />
+</div>
