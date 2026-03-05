1fc1e8578200ae11ec3f2170e8f87f04f61b13ff
HenkieFK <r0997833@student.thomasmore.be>
2026-03-05 13:42:21 +0100

    FK-Dessert bekijken klant+bewerken admin

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -1,11 +1,5 @@
-<div class="flex min-h-screen flex-col">
-    <div class="flex-1">
-        <x-layouts.app.sidebar :title="$title ?? null">
-            <flux:main>
-                {{ $slot }}
-            </flux:main>
-        </x-layouts.app.sidebar>
-    </div>
-
-    <x-layouts.app.footer />
-</div>
+<x-layouts.app.sidebar :title="$title ?? null">
+    <flux:main>
+        {{ $slot }}
+    </flux:main>
+</x-layouts.app.sidebar>
--- resources/views/components/layouts/app/sidebar.blade.php
+++ resources/views/components/layouts/app/sidebar.blade.php
@@ -19,12 +19,20 @@
         <flux:navlist.group :heading="__('Platform')" class="grid">
             <flux:navlist.item icon="home" :href="route('dashboard')" :current="request()->routeIs('dashboard')" wire:navigate>{{ __('Dashboard') }}</flux:navlist.item>
             <flux:navlist.item icon="calendar" href="#" wire:navigate>{{ __('Happenings') }}</flux:navlist.item>
-            <flux:navlist.item icon="cake" href="#" wire:navigate>{{ __('Desserts') }}</flux:navlist.item>
+            <flux:navlist.item icon="cake" :href="route('deserts.index')" :current="request()->routeIs('deserts.index')" wire:navigate>{{ __('Desserts') }}</flux:navlist.item>
+            <flux:navlist.item icon="plus" :href="route('userSurplusShop.index')" :current="request()->routeIs('userSurplusShop.index')" wire:navigate>{{ __('Surpluses') }}</flux:navlist.item>
             <flux:navlist.item icon="shopping-cart" href="#" wire:navigate>{{ __('Orders') }}</flux:navlist.item>
             <flux:navlist.item icon="users" href="#" wire:navigate>{{ __('Workshops') }}</flux:navlist.item>
             <flux:navlist.item icon="clipboard-document-list" href="#" wire:navigate>{{ __('Shopping List') }}</flux:navlist.item>
             <flux:navlist.item icon="star" href="#" wire:navigate>{{ __('Reviews') }}</flux:navlist.item>
         </flux:navlist.group>
+
+        @if(auth()->user()->isAdmin())
+            <flux:navlist.group :heading="__('Beheer')" class="grid mt-4">
+                <flux:navlist.item icon="cake" :href="route('owner.deserts.index')" :current="request()->routeIs('owner.deserts.index')" wire:navigate>{{ __('Desserts Beheren') }}</flux:navlist.item>
+                <flux:navlist.item icon="plus" :href="route('owner.surpluses.index')" :current="request()->routeIs('owner.surpluses.index')" wire:navigate>{{ __('Overschotten Beheren') }}</flux:navlist.item>
+            </flux:navlist.group>
+        @endif
     </flux:navlist>
 
     <!-- Desktop User Menu (pinned to bottom) -->
@@ -59,7 +67,7 @@
                 <flux:menu.separator />
 
                 <flux:menu.radio.group>
-                    <flux:menu.item :href="route('settings.profile')" icon="cog" wire:navigate>{{ __('Settings') }}</flux:menu.item>
+                    <flux:menu.item :href="route('settings.profile')" icon="cog" wire:navigate>{{ __('Settings') }}</flux:menu.menu.item>
                 </flux:menu.radio.group>
 
                 <flux:menu.separator />
@@ -105,24 +113,24 @@
                         </div>
                     </div>
                 </div>
-            </flux:menu.radio.group>
+                </flux:menu.radio.group>
 
-            <flux:menu.separator />
+                <flux:menu.separator />
 
-            <flux:menu.radio.group>
-                <flux:menu.item :href="route('settings.profile')" icon="cog" wire:navigate>{{ __('Settings') }}</flux:menu.item>
-            </flux:menu.radio.group>
+                <flux:menu.radio.group>
+                    <flux:menu.item :href="route('settings.profile')" icon="cog" wire:navigate>{{ __('Settings') }}</flux:menu.item>
+                </flux:menu.radio.group>
 
-            <flux:menu.separator />
+                <flux:menu.separator />
 
-            <form method="POST" action="{{ route('logout') }}" class="w-full">
-                @csrf
-                <flux:menu.item as="button" type="submit" icon="arrow-right-start-on-rectangle" class="w-full">
-                    {{ __('Log Out') }}
-                </flux:menu.item>
-            </form>
-        </flux:menu>
-    </flux:dropdown>
+                <form method="POST" action="{{ route('logout') }}" class="w-full">
+                    @csrf
+                    <flux:menu.item as="button" type="submit" icon="arrow-right-start-on-rectangle" class="w-full">
+                        {{ __('Log Out') }}
+                    </flux:menu.item>
+                </form>
+            </flux:menu>
+        </flux:dropdown>
 </flux:header>
 
 {{ $slot }}
