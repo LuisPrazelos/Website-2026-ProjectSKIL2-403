8eddbddb8571561097987fafec1e98888b1dec30
Eduard Leucea <r1037354@student.thomasmore.be>
2026-03-05 15:40:13 +0100

    Recepten pagina helemaal af gemaakt.

--- routes/web.php
+++ routes/web.php
@@ -50,6 +50,14 @@ Route::middleware(['auth'])->group(function () {
         return view('deserts.index', compact('deserts'));
     })->name('deserts.index');
 
+    // Owner management view for recipes
+    Route::get('/owner/recipes', [RecipeController::class, 'ownerIndex'])->name('owner.recipes.index');
+    Route::post('/owner/recipes', [RecipeController::class, 'store'])->name('owner.recipes.store');
+    Route::get('/owner/recipes/{recipe}', [RecipeController::class, 'show'])->name('owner.recipes.show');
+    Route::get('/owner/recipes/{recipe}/edit', [RecipeController::class, 'edit'])->name('owner.recipes.edit');
+    Route::put('/owner/recipes/{recipe}', [RecipeController::class, 'update'])->name('owner.recipes.update');
+    Route::delete('/owner/recipes/{recipe}', [RecipeController::class, 'destroy'])->name('owner.recipes.destroy');
+
     Route::middleware([AdminMiddleware::class])->group(function () {
         // Owner management view for surpluses
         Route::get('/owner/surpluses', [SurplusController::class, 'ownerIndex'])->name('owner.surpluses.index');
